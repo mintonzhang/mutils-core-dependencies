8ae66503de96221639a4f84f19fd9a11a92fddad
mintonzhang <mintonzhang@163.com>
2020-11-19 11:32:22 +0800

    更新至4.0.1版本

--- mutils-core/src/main/java/cn/minsin/core/vo/VO.java
+++ mutils-core/src/main/java/cn/minsin/core/vo/VO.java
@@ -3,11 +3,10 @@
  */
 package cn.minsin.core.vo;
 
-import cn.minsin.core.exception.MutilsException;
 import com.alibaba.fastjson.JSON;
 
 import java.io.Serializable;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 	动态构建Vo对象 替代的实体类中的VO
@@ -20,74 +19,72 @@ import java.util.HashMap;
  * @date 2019年2月14日
  * @since: 0.0.8.RELEASE
  */
-public class VO extends HashMap<String, Object> implements Serializable {
+public class VO extends ConcurrentHashMap<String, Object> implements Serializable {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 6208106294465026862L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 6208106294465026862L;
 
-    /**
-     * 不推荐使用 仅作为反序列化入口
-     */
-    @Deprecated
-    public VO() {
-    }
+	/**
+	 * 不推荐使用 仅作为反序列化入口
+	 */
+	@Deprecated
+	public VO() {
+	}
 
-    public static VO init() {
-        return new VO();
-    }
+	public static VO init() {
+		return new VO();
+	}
 
-    public static VO init(String key, Object value) {
-        return new VO().put(key, value);
-    }
+	public static VO init(String key, Object value) {
+		return new VO().add(key, value);
+	}
 
-    @Override
-    public VO put(String key, Object value) {
-        super.put(key, value);
-        return this;
-    }
+	/**
+	 * 添加
+	 */
+	public VO add(String key, Object value) {
+		super.put(key, value);
+		return this;
+	}
 
-    public VO remove(String key) {
-        super.remove(key);
-        return this;
-    }
+	public VO remove(String key) {
+		super.remove(key);
+		return this;
+	}
 
-    public VO removeAll() {
-        super.clear();
-        return this;
-    }
+	public VO removeAll() {
+		super.clear();
+		return this;
+	}
 
-    public Object getValue(String key) {
-        return super.get(key);
-    }
+	public Object getValue(String key) {
+		return super.get(key);
+	}
 
-    /**
-     * 	转换成指定对象 必须实现序列化
-     * @param clazz
-     *  指定Class
-     */
-    public <T extends Serializable> T toObject(Class<T> clazz) {
-        try {
-            return JSON.parseObject(this.toString(), clazz);
-        } catch (Exception e) {
-            throw new MutilsException(e, "类型转换失败");
-        }
-    }
+	/**
+	 * 转换成指定对象 必须实现序列化
+	 *
+	 * @param clazz 指定Class
+	 */
+	public <T extends Serializable> T toObject(Class<T> clazz) {
+		return JSON.parseObject(this.toString(), clazz);
+	}
 
 
-    @Override
-    public boolean isEmpty() {
-        return super.isEmpty();
-    }
+	@Override
+	public boolean isEmpty() {
+		return super.isEmpty();
+	}
 
-    @Override
-    public void clear() {
-        super.clear();
-    }
+	@Override
+	public void clear() {
+		super.clear();
+	}
 
-    @Override
-    public String toString() {
-        return JSON.toJSONString(this);
-    }
+	@Override
+	public String toString() {
+		return JSON.toJSONString(this);
+	}
 }
