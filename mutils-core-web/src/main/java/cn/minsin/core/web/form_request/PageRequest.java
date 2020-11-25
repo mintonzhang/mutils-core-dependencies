1b66499ee1070a8256bee3bcd5d6438f69a0e1fe
mintonzhang <mintonzhang@163.com>
2020-11-25 16:00:40 +0800

    复制spring security中的BCrypt加密方式

--- mutils-core-web/src/main/java/cn/minsin/core/web/form_request/PageRequest.java
+++ mutils-core-web/src/main/java/cn/minsin/core/web/form_request/PageRequest.java
@@ -2,8 +2,8 @@ package cn.minsin.core.web.form_request;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
@@ -15,7 +15,7 @@ import javax.validation.constraints.NotNull;
  */
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
+@Setter
 public class PageRequest {
 	/**
 	 * 默认开始页码
@@ -27,46 +27,38 @@ public class PageRequest {
 	private static Integer DEFAULT_SIZE = 10;
 
 	@NotNull
-    @ApiModelProperty("页码,从DEFAULT_PAGE计算")
-    private int page;
+	@ApiModelProperty("页码,从DEFAULT_PAGE计算")
+	private Integer page;
 
-    @NotNull
-    @ApiModelProperty("每页记录数")
-    private int size;
+	@NotNull
+	@ApiModelProperty("每页记录数")
+	private Integer size;
 
-    /**
-     * 设置全局默认条数和页码
-     *
-     * @param defaultPage 默认页码
-     * @param defaultSize 默认显示条数
-     */
-    public static void setGlobalDefaultValue(int defaultPage, int defaultSize) {
-        DEFAULT_PAGE = defaultPage;
-        DEFAULT_SIZE = defaultSize;
-    }
+	/**
+	 * 设置全局默认条数和页码
+	 *
+	 * @param defaultPage 默认页码
+	 * @param defaultSize 默认显示条数
+	 */
+	public static void setGlobalDefaultValue(int defaultPage, int defaultSize) {
+		DEFAULT_PAGE = defaultPage;
+		DEFAULT_SIZE = defaultSize;
+	}
 
+	public Number toPageNumber() {
+		return this.getSize();
+	}
 
-    public void setPage(Integer page) {
-        this.page = page == null || page < DEFAULT_PAGE ? DEFAULT_PAGE : page;
-    }
+	public Number toPageSizeNumber() {
+		return this.getPage();
+	}
 
-    public void setSize(Integer size) {
-        this.size = size == null || size < 1 ? DEFAULT_SIZE : size;
-    }
+	public int getPage() {
+		return this.page = page == null || page < DEFAULT_PAGE ? DEFAULT_PAGE : page;
+	}
 
-
-    /**
-     * long类型的size
-     */
-    public long toLongSize() {
-        return this.getSize();
-    }
-
-    /**
-     * long内息的page
-     */
-    public long toLongPage() {
-        return this.getPage();
-    }
+	public int getSize() {
+		return this.size == null || size < 1 ? DEFAULT_SIZE : size;
+	}
 
 }
--- mutils-core-web/src/main/java/cn/minsin/core/web/result/PageData.java
+++ mutils-core-web/src/main/java/cn/minsin/core/web/result/PageData.java
@@ -25,119 +25,126 @@ import java.util.function.Function;
 @Accessors(chain = true)
 public class PageData<E> implements Serializable {
 
-    private Collection<E> data;
+	private Collection<E> data;
 
-    private int page = 1;
+	private int page = 1;
 
-    private int size = 10;
+	private int size = 10;
 
-    private int totalCount;
+	private int totalCount;
 
-    private int totalPage;
+	private int totalPage;
 
-    //********************************普通方法区****************************************//
-    protected <R> PageData<R> copy(@NonNull Collection<R> data) {
-        return new PageData<R>().setSize(this.size).setPage(this.page).setTotalCount(this.totalCount).setTotalPage(this.totalPage).setData(data);
-    }
+	public static <E> PageData<E> of(int page, int size) {
+		return new PageData<E>().setPage(page).setSize(size);
+	}
 
-    //********************************普通方法区****************************************//
+	public static <E> PageData<E> of(int page, int size, Collection<E> data) {
+		return new PageData<E>().setPage(page).setSize(size).setData(data);
+	}
 
-
-    //********************************匿名空类****************************************//
-
-    private static class EmptyPage extends PageData<Object> {
-
-        @Override
-        public Collection<Object> getData() {
-            return Collections.emptyList();
-        }
-
-        @Override
-        public PageData<Object> setData(Collection<Object> data) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public PageData<Object> setTotalCount(int totalCount) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public PageData<Object> setTotalPage(int totalPage) {
-            throw new UnsupportedOperationException();
-        }
-
-        private static final EmptyPage EMPTY_PAGE;
-
-        static {
-            // 允许修改 page和size  e: Allow modify the page and size.
-            EMPTY_PAGE = new EmptyPage();
-        }
-
-        @SuppressWarnings(SuppressWarningsTypeConstant.UNCHECKED)
-        public static <E> PageData<E> getInstance() {
-            return (PageData<E>) EMPTY_PAGE;
-        }
-    }
-
-    //********************************匿名空类****************************************//
+	//********************************普通方法区****************************************//
 
 
-    //********************************静态方法区****************************************//
+	//********************************匿名空类****************************************//
 
-    public static <E> PageData<E> of(int page, int size) {
-        return new PageData<E>().setPage(page).setSize(size);
-    }
+	public static <E> PageData<E> of() {
+		return new PageData<>();
+	}
 
-    public static <E> PageData<E> of(int page, int size, Collection<E> data) {
-        return new PageData<E>().setPage(page).setSize(size).setData(data);
-    }
-
-    public static <E> PageData<E> of() {
-        return new PageData<>();
-    }
-
-    public static <E> PageData<E> empty() {
-        return EmptyPage.getInstance();
-    }
+	//********************************匿名空类****************************************//
 
 
-    //********************************静态方法区****************************************//
+	//********************************静态方法区****************************************//
+
+	public static <E> PageData<E> empty() {
+		return EmptyPage.getInstance();
+	}
+
+	//********************************普通方法区****************************************//
+	protected <R> PageData<R> copy(@NonNull Collection<R> data) {
+		return new PageData<R>().setSize(this.size).setPage(this.page).setTotalCount(this.totalCount).setTotalPage(this.totalPage).setData(data);
+	}
+
+	/**
+	 * 是否还有下一页
+	 */
+	public boolean getHasMore() {
+		return totalPage > page;
+	}
+
+	/**
+	 * 将E转换成R 并重新生成pageData
+	 *
+	 * @param function 处理函数
+	 */
+	public <R> PageData<R> map(@NonNull Function<E, R> function) {
+		ArrayList<R> newData = Lists.newArrayListWithCapacity(data.size());
+		data.forEach(e -> {
+			R apply = function.apply(e);
+			newData.add(apply);
+		});
+		return this.copy(newData);
+	}
 
 
-    //********************************函数转换、消费区****************************************//
-
-    /**
-     * 将E转换成R 并重新生成pageData
-     *
-     * @param function 处理函数
-     */
-    public <R> PageData<R> map(@NonNull Function<E, R> function) {
-        ArrayList<R> newData = Lists.newArrayListWithCapacity(data.size());
-        data.forEach(e -> {
-            R apply = function.apply(e);
-            newData.add(apply);
-        });
-        return this.copy(newData);
-    }
-
-    /**
-     * 对list进行二次处理
-     *
-     * @param function 处理函数
-     */
-    public PageData<E> forEach(@NonNull Consumer<E> function) {
-        this.data.forEach(function);
-        return this;
-    }
-
-    /**
-     * 传入一个新的List 覆盖原数据
-     */
-    public <N> PageData<N> override(@NonNull Collection<N> collection) {
-        return this.copy(collection);
-    }
+	//********************************静态方法区****************************************//
 
 
-    //********************************函数转换、消费区****************************************//
+	//********************************函数转换、消费区****************************************//
+
+	/**
+	 * 对list进行二次处理
+	 *
+	 * @param function 处理函数
+	 */
+	public PageData<E> forEach(@NonNull Consumer<E> function) {
+		this.data.forEach(function);
+		return this;
+	}
+
+	/**
+	 * 传入一个新的List 覆盖原数据
+	 */
+	public <N> PageData<N> override(@NonNull Collection<N> collection) {
+		return this.copy(collection);
+	}
+
+	private static class EmptyPage extends PageData<Object> {
+
+		private static final EmptyPage EMPTY_PAGE;
+
+		static {
+			// 允许修改 page和size  e: Allow modify the page and size.
+			EMPTY_PAGE = new EmptyPage();
+		}
+
+		@SuppressWarnings(SuppressWarningsTypeConstant.UNCHECKED)
+		public static <E> PageData<E> getInstance() {
+			return (PageData<E>) EMPTY_PAGE;
+		}
+
+		@Override
+		public Collection<Object> getData() {
+			return Collections.emptyList();
+		}
+
+		@Override
+		public PageData<Object> setData(Collection<Object> data) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public PageData<Object> setTotalCount(int totalCount) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public PageData<Object> setTotalPage(int totalPage) {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+
+	//********************************函数转换、消费区****************************************//
 }
