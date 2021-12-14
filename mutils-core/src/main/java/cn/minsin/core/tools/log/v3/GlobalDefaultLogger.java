c9af5fe7eeb8f9321f1e68bc1e5cca2d9ede920b
zhanghang <mintonzhang@163.com>
2021-12-14 10:02:12 +0800

    更新

--- mutils-core/src/main/java/cn/minsin/core/tools/log/v3/GlobalDefaultLogger.java
+++ mutils-core/src/main/java/cn/minsin/core/tools/log/v3/GlobalDefaultLogger.java
@@ -3,10 +3,6 @@ package cn.minsin.core.tools.log.v3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-/**
- * 全局默认日志记录器 仅记录日志
- */
 public interface GlobalDefaultLogger {
 
     Logger log = LoggerFactory.getLogger("LISTENER");
